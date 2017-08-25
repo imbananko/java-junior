93afb0e2f91709b69f7e358acd8c5ec54dfada91
imbananko <imbananko@gmail.com>
2017-08-25 17:54:59 +0300

    DONT OPEN PLEASE!

--- src/main/java/com/acme/edu/formatters/CustomFormatter.java
+++ src/main/java/com/acme/edu/formatters/CustomFormatter.java
@@ -1,6 +1,6 @@
 package com.acme.edu.formatters;
 
-public class StringFormatter extends AbstractFormatter {
+public class CustomFormatter extends AbstractFormatter {
     @Override
     public String format(Object message) {
         return null;
