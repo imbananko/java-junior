07e385121df1ddea1fc5ae008b80241697099609
EugBy <eugene.bykovets@gmail.com>
2017-08-31 10:57:02 +0300

    Want to commit2

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -6,7 +6,7 @@ import com.acme.edu.writers.Writer;
 import java.util.ArrayList;
 
 
-//Want to commit
+//Want to commit2
 public class Logger {
     ArrayList<Object> buffer = new ArrayList<>();
     int previousIndex;
