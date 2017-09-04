066a28b069fd25f00aa397ffbac849247b00eaad
EugBy <eugene.bykovets@gmail.com>
2017-09-04 13:53:42 +0300

    Add test for correctly handling exception

--- src/test/java/com/acme/edu/ConsoleFormatterTest.java
+++ src/test/java/com/acme/edu/ConsoleFormatterTest.java
@@ -7,6 +7,7 @@ import com.acme.edu.handlers.FormattingSavingHandler;
 import com.acme.edu.writers.Writer;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.internal.matchers.Null;
 
 import java.util.ArrayList;
 
@@ -61,4 +62,30 @@ public class ConsoleFormatterTest {
         assertEquals(formatted, "5\n2");
         //endregion
     }
+
+    @Test
+    public void shouldHandleExceptionCorrectlyWhenBufferNullOrBufferEmpty() {
+        // region given
+        String message = null;
+        ArrayList<Object> buffer = null ;
+        //endregion
+
+        //region act
+        try {
+
+            mockedConsoleFormatter.format(null);
+        } catch (FormatterException e) {
+            buffer = e.getBuffer();
+            message = e.getMessage();
+        }
+        //endregion
+
+        //region then
+        assertEquals(message, "Buffer cannot be null");
+        assertEquals(buffer,null);
+        //endregion
+
+
+    }
+
 }
